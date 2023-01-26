5e78922863a998929aad431251dbc358af8ce3fd
DevFrogora <rr4428310@gmail.com>
2023-01-26 15:31:31 +0000

    Client Chat UI

--- FrogChatWebApi/Hubs/ChatHub.cs
+++ FrogChatWebApi/Hubs/ChatHub.cs
@@ -4,10 +4,15 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 //using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 
 namespace FrogChatWebApi.Hubs
 {
+    public static class UserHandler
+    {
+        public static Dictionary<string,int> UserCount = new Dictionary<string,int>();
+    }
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ChatHub :Hub
     {
@@ -27,16 +32,26 @@ namespace FrogChatWebApi.Hubs
 
         public override async Task OnConnectedAsync()
         {
-            UserDto userDto = new UserDto()
+            string userId = Context.User.Claims.Where(claim => claim.Type == "userId").Select(claim => claim.Value).FirstOrDefault();
+            UserHandler.UserCount.TryGetValue(userId, out var founduser);
+            if (founduser == 0)
             {
-                Name = Context.User.Identity.Name,
-                Email = Context.User.Claims.Where(claim => claim.Type == ClaimTypes.Email).Select(claim => claim.Value).FirstOrDefault(),
-                PhotoUrl = Context.User.Claims.Where(claim => claim.Type == "picture").Select(claim => claim.Value).FirstOrDefault(),
-                Username = Context.User.Claims.Where(claim => claim.Type == "username").Select(claim => claim.Value).FirstOrDefault(),
-                Id = Context.User.Claims.Where(claim => claim.Type == "userId").Select(claim => claim.Value).FirstOrDefault(),
-            };
-            chatPersistenceData.users.Add(userDto);
-            //Console.WriteLine(Context.User.Identity.Name);
+                UserHandler.UserCount.Add(userId,1);
+                UserDto userDto = new UserDto()
+                {
+                    Name = Context.User.Identity.Name,
+                    Email = Context.User.Claims.Where(claim => claim.Type == ClaimTypes.Email).Select(claim => claim.Value).FirstOrDefault(),
+                    PhotoUrl = Context.User.Claims.Where(claim => claim.Type == "picture").Select(claim => claim.Value).FirstOrDefault(),
+                    Username = Context.User.Claims.Where(claim => claim.Type == "username").Select(claim => claim.Value).FirstOrDefault(),
+                    Id = userId,
+                };
+                chatPersistenceData.users.Add(userDto);
+                //Console.WriteLine(Context.User.Identity.Name);
+            }
+            else
+            {
+                UserHandler.UserCount[userId] = ++founduser;
+            }
             await Clients.All.SendAsync("Connected", chatPersistenceData.users);
 
             //return base.OnConnectedAsync();
@@ -45,7 +60,17 @@ namespace FrogChatWebApi.Hubs
         public override async Task OnDisconnectedAsync(Exception? exception)
         {
             string Id = Context.User.Claims.Where(claim => claim.Type == "userId").Select(claim => claim.Value).FirstOrDefault();
-            chatPersistenceData.users.Remove(chatPersistenceData.users.Where(user => user.Id.Equals(Id)).FirstOrDefault());
+            UserHandler.UserCount.TryGetValue(Id, out var founduser);
+            if(founduser == 1)
+            {
+                chatPersistenceData.users.Remove(chatPersistenceData.users.Where(user => user.Id.Equals(Id)).FirstOrDefault());
+                UserHandler.UserCount.Remove(Id);
+            }
+            else
+            {
+                UserHandler.UserCount[Id] = --founduser;
+
+            }
             await Clients.All.SendAsync("Disconnected", chatPersistenceData.users);
             //return base.OnDisconnectedAsync(exception);
         }
